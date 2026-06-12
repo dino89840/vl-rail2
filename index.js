cd20089cd5616c1f9775da4d68702707df07c6bc
dino89840 <dino89490@gmail.com>
2026-06-13 02:11:49 +0630

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'rai.samuisajj.cc.cd';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTYzOTJiZjBlZGEzMDdhMDFkN2IwOWQ3NDQ5MGI5NzYiLCJ0IjoiMWY1ZDJlM2YtOWZiNS00MmRiLWE2N2EtMjg5NzhiYTUxZmU2IiwicyI6Ik5ESmtOREk0WkRFdE5qZzBNUzAwTlRBM0xUZzBOVFl0WmpjeU56RXhOamN5WWpsbCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8072;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'www.mytel.com.mm';            // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'www.true.th';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || '';                        // 节点名称
 
